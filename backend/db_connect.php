1b44b1e96d02db9fff6219f6396a75276e7f725b
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-05 12:47:23 +0530

    locally connected

--- backend/db_connect.php
+++ backend/db_connect.php
@@ -1,8 +1,13 @@
 <?php
-$host = "HansilGreat.mysql.pythonanywhere-services.com"; // Your PythonAnywhere MySQL host
-$db_name = "HansilGreat$attendance_system_php_mini"; // Your database name
-$username = "HansilGreat"; // Your PythonAnywhere username
-$password = "hansil714"; // Your MySQL database password
+// $host = "HansilGreat.mysql.pythonanywhere-services.com"; // Your PythonAnywhere MySQL host
+// $db_name = "HansilGreat$attendance_system_php_mini"; // Your database name
+// $username = "HansilGreat"; // Your PythonAnywhere username
+// $password = "hansil714"; // Your MySQL database password
+
+$host = "localhost:3307"; // Your PythonAnywhere MySQL host
+$db_name = "attendance_system_php_mini"; // Your database name
+$username = "root"; // Your PythonAnywhere username
+$password = ""; // Your MySQL database password
 
 $conn = mysqli_connect($host, $username, $password, $db_name);
 
@@ -15,3 +20,5 @@ echo "Connected successfully";
 // $db = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
 // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 // include_once('login.php');
+
+?>
