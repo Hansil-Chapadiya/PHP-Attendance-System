f58363102d36f27c71fa7cd34687fa5e023cce9d
Hansil-Chapadiya <chapadiyahansil@gmail.com>
2024-10-06 12:03:13 +0530

    Locally databases connected'

--- backend/db_connect.php
+++ backend/db_connect.php
@@ -4,10 +4,22 @@
 // $username = "HansilGreat"; // Your PythonAnywhere username
 // $password = "hansil714"; // Your MySQL database password
 
-$host = "localhost:3307"; // Your PythonAnywhere MySQL host
-$db_name = "attendance_system_php_mini"; // Your database name
-$username = "root"; // Your PythonAnywhere username
-$password = ""; // Your MySQL database password
+// $host = "localhost:3307"; // Your PythonAnywhere MySQL host
+// $db_name = "attendance_system_php_mini"; // Your database name
+// $username = "root"; // Your PythonAnywhere username
+// $password = ""; // Your MySQL database password
+
+$host = "sql12.freesqldatabase.com"; // Your PythonAnywhere MySQL host
+$db_name = "sql12735701"; // Your database name
+$username = "sql12735701"; // Your PythonAnywhere username
+$password = "qKJr15MSCX"; // Your MySQL database password
+/*
+Host: sql12.freesqldatabase.com
+Database name: sql12735701
+Database user: sql12735701
+Database password: qKJr15MSCX
+Port number: 3306
+*/
 
 $conn = mysqli_connect($host, $username, $password, $db_name);
 
@@ -15,7 +27,7 @@ $conn = mysqli_connect($host, $username, $password, $db_name);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+// echo "Connected successfully";
 
 // $db = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
 // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
